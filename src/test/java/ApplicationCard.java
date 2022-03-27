68be9e75be9fa92a0cb29e03e08ea4c7f8c59edc
Mikhail Voroshilov <m.s.voroshilovv@gmail.com>
2022-03-27 16:22:11 +0800

    Исправление и теста. проверка по тексту

--- src/test/java/ApplicationCard.java
+++ src/test/java/ApplicationCard.java
@@ -1,17 +1,14 @@
 import com.codeborne.selenide.SelenideElement;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationCard {
     SelenideElement form = $(".form");
@@ -21,54 +18,63 @@ public class ApplicationCard {
         open("http://localhost:9999/");
     }
 
-    @Test
-    public void shouldTestCorrect() {
-        form.$("[data-test-id='name'] input").setValue("Иван Петров-Иванов");
+    @ParameterizedTest
+    @CsvSource({
+            "one name, Анна",
+            "twho, Сергей Васильевич",
+            "thre,Иван Петров-Иванов",
+    })
+    public void shouldTestCorrect(String title, String name) {
+        form.$("[data-test-id='name'] input").setValue(name);
         form.$("[data-test-id='phone'] input").setValue("+79305698778");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
 
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        String message = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        $("[data-test-id=order-success]").shouldHave(exactText(message));
     }
 
     @Test
     public void shouldNotEnterRows() {
-        $(".form").$(".button").click();
-        $("[data-test-id=order-success]").shouldNot(exist);
+        $(".button").click();
+        String error = "Поле обязательно для заполнения";
+        $("[data-test-id=name] .input__sub").shouldHave(exactText(error));
     }
 
     @ParameterizedTest
     @CsvSource({
-            "English, Maik",
-            "spec Simbol, Петр Машк&вич",
-            "underscore, Анна_Сергеевна",
-            "emptiness, ''",
+            "English, Maik, 'Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.'",
+            "spec Simbol, Петр Машк&вич, 'Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.'",
+            "underscore, Анна_Сергеевна, 'Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.'",
+            "emptiness, '', Поле обязательно для заполнения",
     })
-    public void shouldTestIncorrectEnterName(String testName, String name) {
+    public void shouldTestIncorrectEnterName(String testName, String name, String errors) {
         form.$("[data-test-id='name'] input").setValue(name);
         form.$("[data-test-id='phone'] input").setValue("+79305698778");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
 
-        form.$("[data-test-id=name].input_invalid .input__sub").shouldBe(visible);
+        String actual = form.$("[data-test-id=name].input_invalid .input__sub").getText().trim();
+        assertEquals(errors, actual);
     }
 
     @ParameterizedTest
     @CsvSource({
-            "not plus, 79025647891",
-            "over limit, +798541236547",
-            "less than limit, +7984562356",
-            "spec simbol, +7&963256485",
-            "null, ''",
-            "text phone number, +7e965463269",
+            "not plus, 79025647891, 'Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.'",
+            "over limit, +798541236547, 'Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.'",
+            "less than limit, +7984562356, 'Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.'",
+            "spec simbol, +7&963256485, 'Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.'",
+            "text phone number, +7e965463269, 'Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.'",
+            "null, '', Поле обязательно для заполнения",
     })
-    public void shouldTestIncorrectEnterPhone(String testName, String phone) {
+    public void shouldTestIncorrectEnterPhone(String testName, String phone, String errors) {
         form.$("[data-test-id='name'] input").setValue("Аннф-Петровна");
         form.$("[data-test-id='phone'] input").setValue(phone);
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
 
-        form.$("[data-test-id=phone].input_invalid .input__sub").shouldBe(visible);
+        String actual = form.$("[data-test-id=phone].input_invalid .input__sub").getText().trim();
+        assertEquals(errors, actual);
     }
 
     @Test
@@ -77,6 +83,8 @@ public class ApplicationCard {
         form.$("[data-test-id='phone'] input").setValue("+79865923654");
         form.$(".button").click();
 
-        form.$("[data-test-id=agreement].input_invalid .checkbox__text").shouldBe(visible);
+        String actual = form.$("[data-test-id=agreement].input_invalid .checkbox__text").getText().trim();
+        String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+        assertEquals(expected, actual);
     }
 }
